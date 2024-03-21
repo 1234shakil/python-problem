347164f3f1bbf349d9c95505f0659080f9ed13de
Yasin Arafat Shakil <yashakil290905@gamil.com>
2024-03-21 15:15:21 +0600

    while loop 2

---
+++ demo.py
@@ -0,0 +1,21 @@
+
+x = 0
+
+while x <= 9:
+    a = str(x)
+    print(a * x)
+
+    x += 1
+
+
+print()
+print()
+
+x = 0
+while x <= 10:
+    if x%2 != 0:
+        print( f'Odd Number: {x}')
+    else:
+        print(f'Even Number: {x}')
+    x +=1
+ 
